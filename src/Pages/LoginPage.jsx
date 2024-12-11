62f6ee27f7f92810be6507aaa94bdbd01d4a407c
Marion Ginter <gintermarion@hotmail.com>
2024-12-11 16:49:20 +0100

    formulaire inscription input ok

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,40 +1,29 @@
+import { Container, Form } from "react-bootstrap";
 
-import { Button, Container, Form } from "react-bootstrap";
 
 const Login = () => {
-    
-    
-    
-    
-    return <Container className="d-flex flex-column align-items-center">
-    <h1>Connexion</h1>
-    <Form >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Saisir une adresse mail"
-         
-          // value={user.mail}
-           required />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Mot de passe</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Saisir le mot de passe"
-         
-          // value={user.mdp}
-           required />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Se connecter
-      </Button>
+
+
+
+  return <Container className='formulaire-login'>
+    <Form>
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Email</label>
+    </div>
+    <br />
+    <div className="group">
+      <input required="" type="password" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Mot de passe</label>
+    </div>
     </Form>
   </Container>
 
-  
 }
- 
+
 export default Login;
--- src/Pages/SigninPage.jsx
+++ src/Pages/SigninPage.jsx
@@ -10,66 +10,54 @@ const Signin = () => {
 
     <h1>Créer votre profil</h1>
 
-    <Form action="/createUser" method='POST' >
-      <Form.Group className="mb-3 mt-4" controlId="formBasicName">
-        <Form.Label>Nom d'utilisateur</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Saisir un nom/pseudo"
-          //  onChange={handleChange} required
-          name="username"
-        />
-      </Form.Group>
+    <Form>
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Nom d'utilisateur</label>
+    </div>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Adresse mail</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Saisir une adresse mail"
-          //   onChange={handleChange} required
-          name="email"
-        />
-      </Form.Group>
+    <br />
 
-      <Form.Group className="mb-3 " controlId="formBasicPassword">
-        <Form.Label>Mot de passe</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Mot de passe"
-          //   onChange={handleChange} required
-          name="password"
-        />
-      </Form.Group>
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Email</label>
+    </div>
 
-      <Form.Group className="mb-3 " controlId="formBasicLastName">
-        <Form.Label>Nom</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Saisir un nom"
-          //  onChange={handleChange} required
-          name="last_name"
-        />
-      </Form.Group>
+        <br />
 
-      <Form.Group className="mb-3 " controlId="formBasicFirstName">
-        <Form.Label>Prénom</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Saisir un prénom"
-          //  onChange={handleChange} required
-          name="first_name"
-        />
-      </Form.Group>
+    <div className="group">
+      <input required="" type="password" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Mot de passe</label>
+    </div>
+    <br />
 
-      <Form.Group className="mb-3 " controlId="formBasicTel">
-        <Form.Label>Téléphone</Form.Label>
-        <Form.Control
-          type="tel"
-          placeholder="0123456789"
-          //  onChange={handleChange} required
-          name="phone"
-        />
-      </Form.Group>
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Nom</label>
+    </div>
+    <br />
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Prénom</label>
+    </div>
+    <br />
+    <div className="group">
+      <input required="" type="text" className="input"></input>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Téléphone</label>
+    </div>
+    <br />
       <div className="div-btn-create">
         <Button className="form-btn" type="submit">
           Créer utilisateur
