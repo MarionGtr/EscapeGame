896097d5c6b2da0bb3ae421c0ff8d1a5758d8995
Marion Ginter <gintermarion@hotmail.com>
2024-12-12 11:44:22 +0100

    formulaires ok

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Container, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 
 const Login = () => {
@@ -6,7 +6,10 @@ const Login = () => {
 
 
 
-  return <Container className='formulaire-login'>
+  return <Container className='formulaire'>
+
+<h1>Connexion</h1>
+
     <Form>
     <div className="group">
       <input required="" type="text" className="input"></input>
@@ -21,6 +24,11 @@ const Login = () => {
       <span className="bar"></span>
       <label>Mot de passe</label>
     </div>
+    <div className="div-btn-create">
+        <Button className="form-btn" type="submit">
+          Créer utilisateur
+        </Button>
+      </div>
     </Form>
   </Container>
 
