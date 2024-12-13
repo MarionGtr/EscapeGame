12363d475bbcb59fa97dc207a77da7c1e1460f6d
Marion Ginter <gintermarion@hotmail.com>
2024-12-13 12:26:02 +0100

    fonction titre corrigée

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,153 +1,92 @@
-
 import React from "react";
 import MyCarousel from "../Components/Carousel";
 import EscapeCard from "../Components/EscapeCard";
 
-
 const HomePage = () => {
 
-// --------------FONCTION POUR LE TITRE FLOU-----------
+    //---------FONCTION TITRE FLOU-----------//
+  React.useEffect(() => {
+    function setStyle(element, styles) {
+      if (!element) return; // Vérifie si l'élément existe
+      for (const property in styles) {
+        element.style[property] = styles[property];
+      }
+    }
 
-    (function () {
-        var addEvent = function (object, type, callback) {
-            if (object == null || typeof object == "undefined") return;
-            if (object.addEventListener) {
-                object.addEventListener(type, callback, false);
-            } else if (object.attachEvent) {
-                object.attachEvent("on" + type, callback);
-            } else {
-                object["on" + type] = callback;
-            }
-        };
+    // Détecter les mouvements de la souris
+    function detectMouseMove() {
+      const containerTitre = document.querySelector(".container-titre");
+      if (!containerTitre) return; // Vérifie si le conteneur existe
 
-        function getOuterWidth(element) {
-            if (!element) return 0; // Vérifie si l'élément existe
-            const style = getComputedStyle(element);
-            return (
-                element.offsetWidth +
-                parseFloat(style.marginLeft) +
-                parseFloat(style.marginRight)
-            );
-        }
+      const containerWidth = containerTitre.offsetWidth;
+      const hover = document.querySelector(".hover");
+      const leftSide = document.querySelector(".left-side");
+      const rightSide = document.querySelector(".right-side");
 
-        function setStyle(element, styles) {
-            if (!element) return; // Vérifie si l'élément existe
-            for (const property in styles) {
-                element.style[property] = styles[property];
-            }
-        }
+      containerTitre.addEventListener("mousemove", (e) => {
+        const rect = containerTitre.getBoundingClientRect();
+        const pageX = e.clientX - rect.left; // Position relative au conteneur
 
-        function detectMouseMove() {
-            const container = document.querySelector(".container");
-            if (!container) return; // Vérifie si l'élément existe
-            const containerWidth = getOuterWidth(container);
-            const hover = document.querySelector(".hover");
-            const leftSide = document.querySelector(".left-side");
-            const rightSide = document.querySelector(".right-side");
+        setStyle(hover, { left: pageX + "px" });
+        setStyle(leftSide, { left: pageX + 75 + "px" });
+        const rightSideLeft = containerWidth - pageX;
+        setStyle(rightSide, { right: rightSideLeft + "px" });
+      });
+    }
 
-            document.addEventListener("mousemove", function (e) {
-                setStyle(hover, { left: e.pageX + "px" });
-                setStyle(leftSide, { left: e.pageX + 75 + "px" });
-                const rightSideLeft = containerWidth - e.pageX;
-                setStyle(rightSide, { right: rightSideLeft + "px" });
-            });
-        }
+    detectMouseMove(); // Appliquer la détection
 
-        function detectTouch() {
-            const container = document.querySelector(".container");
-            if (!container) return; // Vérifie si l'élément existe
-            const containerWidth = getOuterWidth(container);
-            const hover = document.querySelector(".hover");
-            const leftSide = document.querySelector(".left-side");
-            const rightSide = document.querySelector(".right-side");
+  
+    return () => {
+      const containerTitre = document.querySelector(".container-titre");
+      if (containerTitre) {
+        containerTitre.removeEventListener("mousemove", detectMouseMove);
+      }
+    };
+  }, []);
 
-            document.addEventListener("touchmove", function (e) {
-                const touch = e.touches[0];
-                setStyle(hover, { left: touch.pageX + "px" });
-                setStyle(leftSide, { left: touch.pageX + 75 + "px" });
-                const rightSideLeft = containerWidth - touch.pageX;
-                setStyle(rightSide, { right: rightSideLeft + "px" });
-            });
-        }
+  //--------------------------------------
 
-        function detectInput(inputType) {
-            const container = document.querySelector(".container");
-            if (!container) return; // Vérifie si l'élément existe
-            const hover = document.querySelector(".hover");
-            const leftSide = document.querySelector(".left-side");
-            const rightSide = document.querySelector(".right-side");
-
-            addEvent(window, "resize", function () {
-                const containerWidth = getOuterWidth(container);
-
-                document.addEventListener(inputType, function (e) {
-                    const pageX = inputType === "touchmove" ? e.touches[0].pageX : e.pageX;
-                    setStyle(hover, { left: pageX + "px" });
-                    setStyle(leftSide, { left: pageX + 75 + "px" });
-                    const rightSideLeft = containerWidth - pageX;
-                    setStyle(rightSide, { right: rightSideLeft + "px" });
-                });
-            });
-        }
-
-        detectMouseMove();
-        detectTouch();
-        detectInput("mousemove");
-        detectInput("touchmove");
-    })();
-
-//-------------------------------------
-
-
-    return <>
-        <div className="body-homepage">
-
-            <div className="div-main-title">
-
-                <div id="focus-container" className="container-titre">
-                    <div className="left-side"></div>
-                    <div className="right-side"></div>
-                    <div className="hover">
-                        <div className="tri-1">
-                            <div className="bit-top"></div>
-                            <div className="bit-top-left"></div>
-                        </div>
-                        <div className="tri-2">
-                            <div className="bit-top-right"></div>
-                            <div className="bit-top-right-2"></div>
-                        </div>
-
-                        <div className="tri-3">
-                            <div className="bit-bottom-left"></div>
-                            <div className="bit-bottom-left-2"></div>
-                        </div>
-
-                        <div className="tri-4">
-                            <div className="bit-bottom-right"></div>
-                            <div className="bit-bottom-right-2"></div>
-                        </div>
-
-                    </div>
-                    <p className="blur">ENIGMES EVADEES</p>
-                    <p className="focus">ENIGMES EVADEES</p>
-                </div>
-
+  return (
+    <>
+      <div className="body-homepage">
+        <div className="div-main-title">
+          <div id="focus-container" className="container-titre">
+            <div className="left-side"></div>
+            <div className="right-side"></div>
+            <div className="hover">
+              <div className="tri-1">
+                <div className="bit-top"></div>
+                <div className="bit-top-left"></div>
+              </div>
+              <div className="tri-2">
+                <div className="bit-top-right"></div>
+                <div className="bit-top-right-2"></div>
+              </div>
+              <div className="tri-3">
+                <div className="bit-bottom-left"></div>
+                <div className="bit-bottom-left-2"></div>
+              </div>
+              <div className="tri-4">
+                <div className="bit-bottom-right"></div>
+                <div className="bit-bottom-right-2"></div>
+              </div>
             </div>
-            <div className="bloc-carousel">
-                <div className="div-carousel">
-                    <MyCarousel></MyCarousel>
-
-                </div>
-            </div>
-
-            <div ClassName="div-card">
-                <EscapeCard></EscapeCard>
-            </div>
+            <p className="blur">ENIGMES EVADEES</p>
+            <p className="focus">ENIGMES EVADEES</p>
+          </div>
         </div>
-
-
+        <div className="bloc-carousel">
+          <div className="div-carousel">
+            <MyCarousel />
+          </div>
+        </div>
+        <div className="div-card">
+          <EscapeCard />
+        </div>
+      </div>
     </>
-
-}
+  );
+};
 
 export default HomePage;
